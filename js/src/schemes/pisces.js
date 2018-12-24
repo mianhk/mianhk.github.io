5f2b56d61de02e7e7ebcac89ddf409c59a4ac153
mianhk <gcyu@gmail.com>
2018-12-24 21:01:07 +0800

    Travis CI Auto Builder at 2018-12-24 21:01

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 24 2018 20:03:30 GMT+0800 (China Standard Time)
+// build time:Mon Dec 24 2018 21:01:00 GMT+0800 (China Standard Time)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
